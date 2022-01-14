c3a2257b23044434815739feb97d624a1c8d373e
christk <chrismousmoulas@outlook.com>
2022-01-14 15:01:37 +0200

    test

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
-
+#include <chrono>
+#include <thread>
+// replicas = pods
 int main(int argc, char const *argv[])
 {
-    std::cout << "hello world!" << std::endl;
-    return 0;
+  using namespace std::chrono_literals;
+  std::cout << "Hello waiter\n" << std::flush;
+  while (true)
+  {
+    auto start = std::chrono::high_resolution_clock::now();
+    std::this_thread::sleep_for(2000ms);
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> elapsed = end-start;
+    std::cout << "Waited " << elapsed.count() << " ms\n";
+  }
+  return 0;
 }
